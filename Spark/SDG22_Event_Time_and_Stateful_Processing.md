f2025cc101cf071dd5f30dbd22a0ef37e40f7727
chkao <chkao@amazon.com>
2020-06-08 13:49:30 +0800

    flush SDG / cert notes

--- Spark/SDG22_Event_Time_and_Stateful_Processing.md
+++ Spark/SDG22_Event_Time_and_Stateful_Processing.md
@@ -15,5 +15,61 @@ Structure:
     - flatMapGroupsWithState
 - Conclusion
 
-------------------------------------------
+![alt_txt](https://github.com/kaochikao/jkg_boundary/blob/master/img/windows.png)
+------------------------------------------------------------------------------------
 
+Chapter Bridge: 
+- 前半部：Spark內建的stateful processing
+    - Tumbling & Sliding Windows, Watermark
+- 後半部：Arbitrary Statefule Processing
+    - user 自己定義的window, manage state 的方式．
+    - 有些概念還不好懂
+
+------------------------------------------------------------------------------------
+
+- Viz: Spark在處理stream時不是一個record, 一個record做，而是會buffer一下，然後flush到persistent storage.
+
+Event Time:
+- 想像兩個streaming source, 一個在台灣，一個在美國，同時stream到美國的server, record抵達時間就很有可能不是按event time.
+
+Stateful Processing:
+- Spark stores the `intermediate information` in a `state store`. 
+- Spark’s current state store implementation is an `in-memory state store` that is made `fault tolerant` by storing intermediate state to the `checkpoint directory`.
+    - 主要是in-memory, 但用persistent storage提高reliability.
+
+Arbitrary Stateful Processing:
+- = custom stateful processing
+- 一般的stateful processing是Spark幫你管理．這裡是你自己定義．
+
+
+```python
+from pyspark.sql.functions import col, window
+
+withEventTime\
+    .withWatermark('event_time', '30 minutes')\
+    .groupBy(window(col('event_time'), '10 minutes', '5 minutes'))\
+    .count()\
+    .writeStream\
+    .queryName('example')\
+    .format('memory')\
+    .outputMode('complete')\
+    .start()
+```
+- configure watermark的方式幾乎就跟configure DataFrame read/write option一樣．
+
+```python
+.dropDuplicates(["User", "event_time"])\
+```
+- 要deduplicate也是就直接chain上去．
+
+De-duplication:
+- One of the more difficult operations in record-at-a-time systems is removing duplicates from the stream. Almost by definition, you must operate on a batch of records at a time in order to find duplicates
+    - 沒錯，只看一個record怎麼會知道有重複
+- deduplication是參照user-defined keys?
+
+
+# Arbitrary Stateful Processing
+
+- 上面在講的都是Spark內建的實現, Arbitrary就是user custom的實現．
+- 例子：Maintain `user sessions` of an `undetermined amount of time` and save those sessions to perform some analysis on later.
+    - 這就不是fixed time window.
--- cert/sample.py
+++ cert/sample.py
@@ -1,15 +1,81 @@
+"""
++---+------+--------+
+| id|  type|quantity|
++---+------+--------+
+|  1| apple|      23|
+|  2| apple|     542|
+|  3|banana|      34|
+|  4|banana|      54|
+|  5|banana|      32|
+|  6|orange|      76|
+|  7|cherry|    null|
+|  8|  null|      90|
++---+------+--------+
+"""
+
+"""
+>>> spark.range(1, 7, 2).show()
++---+
+| id|
++---+
+|  1|
+|  3|
+|  5|
++---+
+"""
+
+from pyspark.sql import functions as F
+from pyspark.sql.types import *
+# --------------------------------------------------------------------------------------------------
+
+df = spark.read.option('header', 'true').csv(data_dir + 'dummy', inferSchema=True)
+df = spark.createDataFrame([('Tom', 80), (None, 60), ('Alice', None)], ["name", "height"])
+# --------------------------------------------------------------------------------------------------
 
 
+schema = StructType([
+    StructField("id", IntegerType()),
+    StructField("type", StringType()),
+    StructField("quantity", IntegerType())
+])
+
+df = spark.read.option('header', 'true').csv(data_dir + 'dummy', schema=schema)
+# --------------------------------------------------------------------------------------------------
+
+# Partition
 # TODO: foreachPartition
-
 # TODO: sortWithinPartitions
+df.rdd.getNumPartitions()
+df.select('*', F.spark_partition_id().alias("pid")).show()
+
+df.where(df.type == 'apple').show() # where = filter
+
+# Select
+df.select('*', 'id').show()
+df.select(f.concat(df.id, F.lit('-'), df.type).alias('s')).show()
+df.select(f.format_string('%d-%s', df.id, df.type).alias('test_foramt')).show()
+df.select(df.id.cast(StringType())).printSchema()
+df.select("*", F.when(df.quantity > 50, 'High').when(df.quantity < 30, 'Low').otherwise('Medium').name('Q')).show()
+
+# Filter
+df.filter(df.type.contains('a')).show()
+df.filter(df.type.like('app%')).show()
 
 
-# TODO: spark_partition_id
-# df.repartition(1).select(spark_partition_id().alias("pid")).collect()
+# Columns
+df.withColumn('today', F.current_date())
+df.withColumnRenamed('type', 'new_type').show()
 
-# TODO: groupby
+# Aggregations
+# TODO: crosstab
+# TODO: cube
+# TODO: join
+df.groupBy('id', 'type').sum('quantity').show()
+df.agg(f.countDistinct(df.id, df.type).alias('unique')).show()
 
-# TODO: rename column
 
 # TODO: initialize a spark session
+
+# handle missing values
+df.na.drop().show()
+df.na.fill('-').show()
